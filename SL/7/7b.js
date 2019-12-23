3f6f8fa687bf64896082f0179f666cdb82b6b05f
Saba Khan <saba100.ksk@gmail.com>
2019-12-23 12:01:14 +0530

    changes

--- SL/7/7b.js
+++ SL/7/7b.js
@@ -1,53 +1,48 @@
 window.onload=()=>{
     cars=[
         {
-            "model":"BMW",
-            "name":"modelS",
-            "price":65743,
+            "model":"Lamborghini Aventador",
+            "name":"Lamborghini",
+            "price":"789,809",
             "year":2016
         },
         {
-            "model": "FIAT ",
-            "name": "modelX",
-            "price": 83700,
+            "model": "Tesla Model X ",
+            "name": "Tesla",
+            "price": "104,990",
             "year": 2017
         },
         {
-            "model": "FORD",
-            "name": "model3",
-            "price": 35000,
-            "year": 2019
+            "model": "Porche 918 Spyder",
+            "name": "Porche",
+            "price": "845,000",
+            "year": 2017
         },
     ]
 
     cars.forEach(function(item,index){
         th=document.createElement('th')
-        th.id=item.model
-        th.innerHTML=item.model
+        th.id=item.name
+        th.innerHTML=item.name
         document.getElementById('menu').appendChild(th)
 
     })
 
     cars.forEach(function(item,index){
-        s=document.getElementById(item.model)
+        s=document.getElementById(item.name)
         s.onmouseover=(function(){
-            car=item
-            if(item){
                 document.getElementById('detail').removeAttribute('hidden')
-                document.getElementById(item.model).style.backgroundColor='pink'
+                document.getElementById(item.name).style.backgroundColor='pink'
                 document.getElementById('name').innerHTML=item.model
                 document.getElementById('img').innerHTML='<img src="img/'+item.name+'.jpeg" alt="car"/>'
                 document.getElementById('cost').innerHTML="$"+item.price
-                document.getElementById('yr').innerHTML=item.year
-    
-            }
-            
+                document.getElementById('yr').innerHTML=item.year           
         })
 
         s.onmouseout=function(){
             car=item
             if(item){
-                document.getElementById(item.model).style.backgroundColor='blueviolet'
+                document.getElementById(item.name).style.backgroundColor='blueviolet'
                 document.getElementById('detail').toggleAttribute('hidden')
 
             }
